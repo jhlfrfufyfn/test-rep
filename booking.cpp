2095df4d352d70b05d2be7c96064462260232ecf
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2020-06-26 01:12:58 +0300

    Accepted solution

--- booking.cpp
+++ booking.cpp
@@ -3,45 +3,58 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
+#include <ios>
 #include <tuple>
 
+typedef long long ll;
+#define int ll
 std::unordered_map<std::string, int> roomsTaken;
 std::vector<std::tuple<int, std::string, int, int>> brones;
 std::unordered_map<std::string, std::set<std::pair<int, int> > > clientsInHotel;
 
-int main() {
+signed main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
     int q;
     std::cin >> q;
+
     int p = 0;
+
     while (q--) {
         std::string type;
         std::cin >> type;
+
         if (type == "BOOK") {
             int time, client_id, room_count;
             std::string hotel_name;
             std::cin >> time >> hotel_name >> client_id >> room_count;
+
             auto t = std::make_tuple(time, hotel_name, client_id, room_count);
             brones.push_back(t);
             roomsTaken[hotel_name] += room_count;
+
             auto it = clientsInHotel[hotel_name].upper_bound({client_id, -1});
             int count = it->second;
             if (it == clientsInHotel[hotel_name].end() || it->first != client_id) {
                 it = clientsInHotel[hotel_name].insert({client_id, 0}).first;
+                count = 0;
             }
             clientsInHotel[hotel_name].erase(it);
             clientsInHotel[hotel_name].insert({client_id, count + 1});
-            while (std::get<0>(brones[p]) >= time) {
+
+            while (time - 86400ll >= std::get<0>(brones[p]) && p != (int) brones.size() - 1) {
                 int oldClient = std::get<2>(brones[p]);
                 int oldRoomCount = std::get<3>(brones[p]);
                 std::string oldHotel = std::get<1>(brones[p]);
                 roomsTaken[oldHotel] -= oldRoomCount;
+
                 auto oldIt = clientsInHotel[oldHotel].upper_bound({oldClient, -1});
                 int count = oldIt->second;
-                if (oldIt == clientsInHotel[oldHotel].end() || oldIt->first != oldClient) {
-                    oldIt = clientsInHotel[oldHotel].insert({oldClient, 0}).first;
+                clientsInHotel[oldHotel].erase(oldIt);
+                if (count - 1 > 0) {
+                    clientsInHotel[oldHotel].insert({oldClient, count - 1});
                 }
-                clientsInHotel[hotel_name].erase(oldIt);
-                clientsInHotel[hotel_name].insert({client_id, count + 1});
+                p++;
             }
         } else if (type == "CLIENTS") {
             std::string hotel;
@@ -53,4 +66,5 @@ int main() {
             std::cout << roomsTaken[hotel] << "\n";
         }
     }
+    return 0;
 }
